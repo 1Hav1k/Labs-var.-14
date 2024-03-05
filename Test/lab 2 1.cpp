ac92e6a356e03f7570cbeaf5c435fa981fc21163
Hav1k <antonsklyarov27@gmail.com>
2024-03-05 14:57:36 +0300

    Lab №2 1

---
+++ Test/lab 2 1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    int N;
+    cout << "Введите размер массива: ";
+    cin >> N;
+    if (cin.fail())
+    {
+        cout << "Ошибка ввода данных" << endl;
+        return 1;
+    }
+    else
+    {
+        int A[N];
+        int *p;
+        p = &A[0];
+        srand(time(NULL));
+        cout << "Исходный массив: ";
+        for (int i = 0; i < N; i++)
+        {
+            *(p + i) = rand() % 200 - 100;
+            cout << *(p + i) << " ";
+        }
+        cout << endl;
+        int count = 0;
+        cout << "Номера элементов, больших своего правого соседа: ";
+        for (int i = 0; i < N - 1; i++) {
+            if (*(p + i) > *(p + i + 1)) {
+                cout << i + 1 << " ";
+                count++;
+            }
+        }
+        cout << endl;
+        if (count == 0) {
+            cout << "Таких элементов нет" << endl;
+        } else {
+            cout << "Количество элементов, больших своего правого соседа: " << count << endl;
+        }
+        return 0;
+    }
+}
---
+++ Test/lab 2 2.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main1()
+{
+    int n;
+    cout << "Введите размер массива: ";
+    cin >> n;
+    if ((cin.fail()) or (n < 1))
+        {
+        cout << "Ошибка ввода данных";
+        return 1;
+        }
+    else {
+        double *arr = new double[n];
+        double *p = arr;
+        double *min_element = arr;
+        cout << "Исходный массив: ";
+        srand(time(NULL));
+        for (int i = 0; i < n; i++)
+            {
+                *(p + i) = (float)(rand()% 4 - 2) / (float)(rand()% 4 - 2);
+                cout << *(p + i) << " ";
+            if (*(p + i) < *min_element)
+                {
+                min_element = p + i;
+                }
+            }
+        
+        cout << endl;
+        cout << "Номер минимального элемента: " << (min_element - arr) + 1 << endl;
+
+        double *first_neg = nullptr;
+        double *second_neg = nullptr;
+
+        for (double *p = arr; p < arr + n; p++)
+        {
+            if (*p < 0)
+            {
+                if (first_neg == nullptr)
+                {
+                    first_neg = p;
+                }
+                else if (second_neg == nullptr)
+                {
+                    second_neg = p;
+                    break;
+                }
+            }
+        }
+
+        if (first_neg == nullptr or second_neg == nullptr)
+        {
+            cout << "Недостаточно отрицательных элементов в массиве" << endl;
+        }
+        else
+        {
+            double sum = 0;
+            for (double *p = first_neg + 1; p < second_neg; p++)
+            {
+                sum += *p;
+            }
+            cout << "Сумма элементов между первым и вторым отрицательными элементами: " << sum << endl;
+        }
+
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (abs(*(arr + i)) <= 1)
+            {
+                double temp = *(arr + j);
+                *(arr + j) = *(arr + i);
+                *(arr + i) = temp;
+                j++;
+            }
+        }
+
+        cout << "Новый массив: ";
+        for (int i = 0; i < n; i++)
+        {
+            cout << *(arr + i) << " ";
+        }
+
+        delete[] arr;
+
+        return 0;
+    }
+
+}
